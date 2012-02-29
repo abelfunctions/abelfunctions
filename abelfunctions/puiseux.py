7ddab0463dc036525c782cbb4b755f58ea28ab29
Chris Swierczewski <cswiercz@gmail.com>
2012-02-29 15:47:16 -0800

    Compute leading terms of Puiseux series in sympy. Some __main__ tests in puiseux.py.

--- abelfunctions/puiseux.py
+++ abelfunctions/puiseux.py
@@ -4,6 +4,8 @@ from __future__ import division
 Puiseux Series
 ==============
 
+Tools for computing Puiseux series. A necessary component to computing 
+integral bases and with Riemann surfaces.
 """
 
 ###############################################################################
@@ -84,7 +86,8 @@ def _qmluv(side):
 
     # determine q,m,l from slope
     slope = sympy.Rational(p2[1]-p1[1], p2[0]-p1[0])
-    q = slope.q; m = -slope.p; l = s*p1[1] - r*p1[0]
+    q = slope.q; m = -slope.p
+    l = q*p1[1] + m*p1[0]
 
     # use xgcd to compute u,v such that qu-mv=1
     u,v,g = sympy.gcdex(q,-m)
@@ -103,10 +106,11 @@ def newton_puiseux(f,x,y):
     L = []
     eps = 1e-14
     T = sympy.symbols('T')
+    p = sympy.Poly(f)
     
     # obtain some coefficient data and compute the newton
     # polygon of f. (The lower convex hull of the support.)
-    aij = _coefficient_data(f)
+    aij = _coefficient_data(p)
     support = a.keys()
     newton = newton_polygon(support)
     
@@ -126,8 +130,8 @@ def newton_puiseux(f,x,y):
         # slopes. append all colinear points to side
         for kk in xrange(2,N-n-1):
             pt = newton[n+k]
-            slope = (pt[1]-size[0][1]) / (pt[0]-size[0][0])
-            if abs(slope - base_slope) < eps:
+            slope = (pt[1]-side[0][1]) / (pt[0]-side[0][0])
+            if abs(slope - baseslope) < eps:
                 side.append(pt)
             else:
                 k = kk
@@ -138,11 +142,24 @@ def newton_puiseux(f,x,y):
         # and the characteristic polynomial
         q,m,l,u,v = _qmluv(side)
         i0 = min(side, key=itemgetter(0))[0]
-        phi = sum(aij[(i,j)]*T**Rational(i-i0,q) for (i,j) in side)
-        
+        phi = sum(aij[(i,j)]*T**sympy.Rational(i-i0,q) for (i,j) in side)
+        rts = sympy.roots(phi)
 
-            
+        for xi,M in rts.iteritems():
+            # compute the polynomial H. 
+            H = f.subs({x:(xi**v*x**q), y:(xi**u*x**m*(1+y))}).simplify()
 
+            # recurse when we reach a multiple root
+            if M == 1: L1 = [[H,x,y]]
+            else:      L1 = newton_puiseux(H,x,y)
+
+            # append Puiseux data
+            for G,P,Q in L1:
+                L.append([G,
+                          (xi**v*P**q).simplify(),
+                          (xi**u*P**m*(1+Q)).simplify()])
+
+    return L
 
     
 
@@ -153,20 +170,33 @@ if __name__ == "__main__":
     print "==== Test Suite: puiseux.py ==="
 
     # example algebraic curve
-    x,y = sympy.symbols('x,y')
-    f   = sympy.poly(x**4 + x**2*y**2 - 2*x**2*y - y**2*x + y**2)
-    #f = sympy.poly(-y**5+(2*x-1)*y**4+(3*x-x**2)*y**3+(x-3*x**2)*y**2+(x**3-2*x**2)*y+x**6)
+    x,y,T = sympy.symbols('x,y,T')
+    f1 = -y**5 + (2*x-1)*y**4 - (3*x-x**2)*y**3 - (x-3*x**2)*y**2 + \
+         (x**3-2*x**2)*y + x**6
+    f2 = y**2 - x**2*(x+1)
+    f3 = 0
+
+    f = f2
+    print "Curve:"
+    print 
+    print "\t", f
+    print
     
     # display coefficient data, support, and newton polygon
-    a = _coefficient_data(f)
+    a = _coefficient_data(sympy.Poly(f))
     support = a.keys()
     newton = newton_polygon(support)
     print "Newton Polygon:"
-    print "  f =", f
-    print "  a =", a
-    print
     print "  support =", support
     print "  newton  =", newton
-    print "  Polygon(newton) =", sympy.Polygon(*newton)
+    print
     
+    # compute puiseux series
+    print "Puiseux series:"
+    L = newton_puiseux(f,x,y)
+    for G,P,Q in L:
+        print "  P =", P.subs(x,T)
+        print "  Q =", Q
+        print
+        
 
