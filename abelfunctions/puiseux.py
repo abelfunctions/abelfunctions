44084e22b639c2e16e10810aa79b9acf0828b7e7
Chris Swierczewski <cswiercz@gmail.com>
2012-03-01 10:56:34 -0800

    Puiseux seems stable aside from knowing how to combine [Gi,Pi,Qi] data.

--- abelfunctions/puiseux.py
+++ abelfunctions/puiseux.py
@@ -1,4 +1,4 @@
-from __future__ import division
+#from __future__ import division
 
 """
 Puiseux Series
@@ -8,17 +8,11 @@ Tools for computing Puiseux series. A necessary component to computing
 integral bases and with Riemann surfaces.
 """
 
-###############################################################################
-#
-# 
-#
-###############################################################################
-
 import numpy
 import scipy
 import sympy
-
 from operator import itemgetter
+
 
 def _coefficient_data(f):
     """
@@ -63,7 +57,7 @@ def newton_polygon(support):
     # find the start and end points (0,J) and (I,0)
     JJ = min([j for (i,j) in hull if i == 0])
     II = min([i for (i,j) in hull if j == 0])
-    testslope = -JJ/II
+    testslope = -float(JJ)/II
     
     for (i,j) in hull_with_bdry:
         # when the point is on the j-axis, the onle one we want is the
@@ -71,7 +65,7 @@ def newton_polygon(support):
         if i == 0:
             if j == JJ: slope = -sympy.oo
             else:       slope = sympy.oo
-        else:           slope = (j-JJ)/i     
+        else:           slope = float(j-JJ)/i     
         # equality is for the case when the point is (II,0)
         if slope <= testslope: newton.append((i,j))
             
@@ -105,13 +99,13 @@ def newton_puiseux(f,x,y):
     # create a container for the triplets
     L = []
     eps = 1e-14
-    T = sympy.symbols('T')
-    p = sympy.Poly(f)
+    T = sympy.Symbol('T')
     
     # obtain some coefficient data and compute the newton
     # polygon of f. (The lower convex hull of the support.)
+    p = sympy.poly(f,x,y)
     aij = _coefficient_data(p)
-    support = a.keys()
+    support = aij.keys()
     newton = newton_polygon(support)
     
     # loop over each side. compute:
@@ -123,14 +117,14 @@ def newton_puiseux(f,x,y):
     while n < N-1:
         # determine slope of current line
         side = [newton[n],newton[n+1]]
-        baseslope = (side[1][1]-side[0][1]) / (side[1][0]-side[0][0])
+        baseslope = float(side[1][1]-side[0][1]) / (side[1][0]-side[0][0])
         k = 2
 
         # check against all following points for colinearity by comparing
         # slopes. append all colinear points to side
         for kk in xrange(2,N-n-1):
             pt = newton[n+k]
-            slope = (pt[1]-side[0][1]) / (pt[0]-side[0][0])
+            slope = float(pt[1]-side[0][1]) / (pt[0]-side[0][0])
             if abs(slope - baseslope) < eps:
                 side.append(pt)
             else:
@@ -147,7 +141,8 @@ def newton_puiseux(f,x,y):
 
         for xi,M in rts.iteritems():
             # compute the polynomial H. 
-            H = f.subs({x:(xi**v*x**q), y:(xi**u*x**m*(1+y))}).simplify()
+            H = f.subs({x:(xi**v*x**q), y:(xi**u*x**m*(1+y))})
+            H = (H/(x**l)).simplify()
 
             # recurse when we reach a multiple root
             if M == 1: L1 = [[H,x,y]]
@@ -161,6 +156,46 @@ def newton_puiseux(f,x,y):
 
     return L
 
+
+def puiseux(f,x,y,a,n,T=True):
+    """
+    Compute the degree `n` Puiseux series of `f`.
+    """
+    from sympy.abc import T
+
+    # perform shift
+    if a == sympy.oo: f = f.subs(x,1/x)
+    else:             f = f.subs(x,x+a)
+
+    # stack for keeping track of Puiseux terms
+    stack = newton_puiseux(f,x,y)
+    series = []
+
+    while len(stack) > 0:
+        # 1) pop last element of stack and check degree of series
+        G,P,Q = stack.pop()
+        p = sympy.poly(P,x,y)
+        q = sympy.poly(Q,x,y)
+        deg = float(q.degree(x)) / p.degree(x)
+
+        if deg >= n:
+            # 2a) if degree exceeds target degree, n, then add to storage
+            #     list "series" and pop from stack
+            P = P.subs(x,T)
+            Q = Q.subs({x:T,y:0})
+            series.append((P,Q))
+        else:
+            # 2b) otherwise, compute terms following these and extend the stack
+            next_terms = newton_puiseux(G,x,y)
+            extended_series = []
+            for G1,P1,Q1 in next_terms:
+                ext = [G1,P.subs(x,P1),Q.subs({y:Q1})]
+                extended_series.append(ext)
+            stack.extend(extended_series)
+
+    return series
+
+        
     
 
 """
@@ -168,15 +203,14 @@ TESTS
 """
 if __name__ == "__main__":
     print "==== Test Suite: puiseux.py ==="
-
     # example algebraic curve
     x,y,T = sympy.symbols('x,y,T')
     f1 = -y**5 + (2*x-1)*y**4 - (3*x-x**2)*y**3 - (x-3*x**2)*y**2 + \
          (x**3-2*x**2)*y + x**6
     f2 = y**2 - x**2*(x+1)
     f3 = 0
+    f  = f2
 
-    f = f2
     print "Curve:"
     print 
     print "\t", f
@@ -191,12 +225,25 @@ if __name__ == "__main__":
     print "  newton  =", newton
     print
     
-    # compute puiseux series
-    print "Puiseux series:"
+    # compute leading terms
+    print "Leading terms in Puiseux series:"
     L = newton_puiseux(f,x,y)
     for G,P,Q in L:
         print "  P =", P.subs(x,T)
-        print "  Q =", Q
+        print "  Q =", Q.subs(x,T)
         print
-        
+        L1 = newton_puiseux(G,x,y)
+        for G1,P1,Q1 in L1:
+            print "    P1 =", P1.subs(x,T)
+            print "    Q1 =", Q1.subs(x,T)
+            print
+            
+    # compute Puiseux series
+    print "Puiseux series:"
+    p = puiseux(f,x,y,0,2)
+    print
+    for p1 in p:
+        print "\tP =", p1[0].expand()
+        print "\tQ =", p1[1].expand()
+        print
 
