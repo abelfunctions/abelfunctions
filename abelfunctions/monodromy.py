bef012e3c8ebc560505986438b4d76dd1cbfb7cf
Chris Swierczewski <cswiercz@gmail.com>
2012-09-04 08:27:46 -0700

    Monodromy checkpoint.

--- abelfunctions/monodromy.py
+++ abelfunctions/monodromy.py
@@ -14,36 +14,33 @@ from matplotlib.patches import Circle, Wedge
 from matplotlib.lines import Line2D
 from matplotlib.collections import PatchCollection
 
-import pdb
-
 class cachedmethod(object):
-   """
-   Decorator. Caches a function's return value each time it is called.
-   If called later with the same arguments, the cached value is returned 
-   (not reevaluated).
-   """
-   def __init__(self, func):
-      self.func = func
-      self.cache = {}
-   def __call__(self, *args, **kwds):
-      try:
-         return self.cache[(args,kwds)]
-      except KeyError:
-         value = self.func(*args,**kwds)
-         self.cache[(args,kwds)] = value
-         return value
-      except TypeError:
-         # uncachable -- for instance, passing a list as an argument.
-         # Better to not cache than to blow up entirely.
-         return self.func(*args,**kwds)
-   def __repr__(self):
-      return self.func.__doc__
-   def __get__(self, obj, objtype):
-      return functools.partial(self.__call__, obj)
+    """
+    Decorator. Caches a function's return value each time it is called.
+    If called later with the same arguments, the cached value is returned 
+    (not reevaluated).
+    """
+    def __init__(self, func):
+        self.func = func
+        self.cache = {}
+    def __call__(self, *args, **kwds):
+        try:
+            return self.cache[(args,kwds)]
+        except KeyError:
+            value = self.func(*args,**kwds)
+            self.cache[(args,kwds)] = value
+            return value
+        except TypeError:
+            # uncachable -- for instance, passing a list as an argument.
+            # Better to not cache than to blow up entirely.
+            return self.func(*args,**kwds)
+    def __repr__(self):
+        return self.func.__doc__
+    def __get__(self, obj, objtype):
+        return functools.partial(self.__call__, obj)
 
 
-
-class Permutation:
+class Permutation(object):
     """
     A permutation.
 
@@ -179,6 +176,7 @@ def matching_permutation(a, b):
     return Permutation(perm)
 
 
+
 def prim_fringe(G, weight_function=lambda e: 1, starting_vertex=None):
     """
     Computes a minimal spanning tree of the graph G. Variant of Prim's 
@@ -211,53 +209,80 @@ def prim_fringe(G, weight_function=lambda e: 1, starting_vertex=None):
 
 
 
-
-class Monodromy:
-    def __init__(self, f, x, y):
+class Monodromy(object):
+    def __init__(self, f, x, y, dtype=np.complex, kappa=1/2.9):
         """
         The monodromy group corresponding to the complex plane algebraic curve
         `f = f(x,y)`.
+
+        Inputs:
+        
+        -- f,x,y: a Sympy/Sage plane algebraic curve
+
+        -- dtype: (default: numpy.complex) a Numpy dtype where all numerical
+        computations are to take place
         """
         self.f = f
         self.x = x
         self.y = y
-
-        self.kappa = None
-
+        
+        self.kappa = kappa
+        self.dtype = dtype
+        
+        self.monodromy_graph = None
+        self._compute_monodromy_graph()
+        
+        
     def __repr__(self):
         return "Monodromy group of the Riemann surface defined by the algebraic curve %s." %(self.f)
 
-    
-        
+    def __str__(self):
+        return self.__repr__()
+
+    def base_point(self):
+        return self.graph.data['base_point']
+
+    def sheets(self):
+        return self.graph.data['sheets']
+
+    @cachedmethod
+    def branch_points(self):
+        """
+        Returns the branch points of the algebraic curve. These are the
+        discriminant points that have non-identity monodromy elements.
+        """
+        pass
+
+    @cachedmethod
+    def permutations(self):
+        """
+        Returns the monodromy group: a list of branch points and their 
+        corresponding permutations.
+        """
+        pass
+
     @cachedmethod
     def discriminant_points(self):
         """
         Computes a list of the  discriminant points of a plane algebraic 
         curve `f = f(x,y)`
         """
+        f, x, y = self.f, self.x, self.y
         resultant = sy.resultant(f,f.diff(y),y)
-        return sy.roots(resultant,x).keys()
-
+        disc_pts = sy.roots(resultant,x).keys()
+        return disc_pts
 
     @cachedmethod
-    def monodromy_radius(self, kappa=1/2.9):
+    def monodromy_radius(self):
         """
         Helper function for ``monodromy``.
-
+        
         Returns the radius of the circles contained in the initial 
         monodromy paths.
-
-        INPUTS:
-
-        - ``kappa``: a relaxation factor. Decreasing `\kappa` decreases the radius
-        of the circles. Setting `\kappa = 1` would cause the circles to touch, 
-        which is not recommended for computational purposes.
         """
-        self.kappa = kappa
-
-        min_rho = 2**(-np.nbytes[self.dtype]*8)      # minimal bits of precision
+        min_rho = 2**(-np.nbytes[self.dtype]*4)      # minimal bits of precision
         rho     = sy.oo
-        b       = [np.complex(pt) for pt in self.discriminant_points()]
+        b       = [self.dtype(pt) for pt in self.discriminant_points()]
         for b1 in b:
             for b2 in b:
                 if b1 != b2:
@@ -265,131 +290,109 @@ class Monodromy:
                     if dist < rho: rho = dist
 
         if rho < min_rho:
-            warnings.warn("Cannot compute monodromy: discriminant points are too close and may cause numerical errors.",
+            warnings.warn("Cannot compute monodromy: discriminant points " + \
+                          "are too close and may cause numerical errors.",
                           RuntimeWarning)
 
-        radius = np.double(kappa*rho/2.0)
+        radius = self.dtype(self.kappa*rho/2.0).real
         return radius
         
-
-    def initial_monodromy(self, kappa=1/2.9):
+    def _compute_monodromy_graph(self):
         """
-        Compute the initial monodromy paths and "pathinds".
+        Constructs a NetworkX graph describing the path connectedness of the
+        monodromy paths. Call several subroutines to compute:
 
-        The paths are represented by a list of tuples, each tuple representing a 
-        line from one (labeled) discriminant point to another. That is `[i,j]` 
-        indicates a straight line path from the `i`th discriminant point to the 
-        `j`th discriminant point.
-
-        The variable ``pathinds`` indicates the starting and 
-        ending orientations of the paths on the path circles. That is, if 
-        `[i,j]` is a path and, say, `[1,-1]` is its corresponding "``pathind``",
-        this indicates that the path starts on the rightmost point on the circle
-        about the `i`th discriminant point and ends on the leftmost point on the 
-        circle about the `j`th discriminant point.
-
-        INPUTS:
-
-        - ``kappa``: a relaxation factor. Decreasing `\kappa` decreases the 
-        radius of the circles. Setting `\kappa = 1` would cause the circles 
-        to touch, which is not recommended for computational purposes.
-
-        OUTPUTS:
-
-        - ``H``: minimal spanning tree
-
-        - ``disc_pts``: discriminant points sorted according to [FKS]
-
-        - ``paths``: minimal spanning tree paths as described above
-
-        - ``pathinds``: path indicators, as described above
+        * paths -- compute path indicators
+        * interpolating_points -- computes interpolating points along each
+          edge and at each node (both upper and lower points on circle)
+        * 
         """
         # convert discriminant points to complex floats for performance
-        radius = self.monodromy_radius(kappa=kappa)
-        disc_pts = np.array(disc_pts, dtype=np.complex).tolist()
+        radius = self.monodromy_radius()
+        disc_pts = np.array(self.discriminant_points(), dtype=self.dtype)
+
+        def cmp(z):
+            return np.angle(z-base_point)
 
         # Compute starting point and reorder discriminant points
         key        = lambda z: 100*np.real(z) - np.imag(z)
         starting_vertex_pos = min(disc_pts, key=key)
         base_point = np.complex(starting_vertex_pos - radius)
-
-        def cmp(z):
-            return np.angle(z-base_point)
-
-        disc_pts.sort(key=cmp)
+        disc_pts = sorted(disc_pts, key=cmp)
         starting_vertex = disc_pts.index(starting_vertex_pos)
 
         # Compute minimal spanning tree. the weights are the distances 
         # between the nodes. the spanning tree algorihtm is from a Sage
         # implementation of Prim with a designated starting node since
         n = len(disc_pts)
-        G = nx.complete_graph(n)
-        weight_function = lambda e: abs(disc_pts[e[1]] - disc_pts[e[0]])
-        spanning_tree = prim_fringe(G, weight_function=weight_function, 
+        GG = nx.complete_graph(n)
+        weight_function = lambda e: np.abs(disc_pts[e[1]] - disc_pts[e[0]])
+        spanning_tree = prim_fringe(GG, weight_function=weight_function, 
                                     starting_vertex=starting_vertex)
-        H = nx.Graph()
-        H.add_edges_from(spanning_tree)
+        G = nx.Graph()
+        G.add_edges_from(spanning_tree)
+        G.data['starting_vertex'] = starting_vertex
+        G.data['base_point'] = base_point
 
+        G = self._compute_monodromy_graph_paths(G)
+        G = self._compute_monodromy_graph_vertex_types(G)
+        G = self._compute_monodromy_graph_interpolating_points(G)
+
+        return G
+
+
+    def _compute_monodromy_graph_paths(G):
+        """
+        Compute path connectedness between each vertex.
+        """
         # add node positions
-        pos = {}
-        for i in H.nodes_iter():
-            b = np.complex(disc_pts[i])
-            pos[i] = (np.real(b), np.imag(b))
-            H.node[i]['pos'] = pos[i]
+        for n in G.nodes_iter():
+            b = disc_pts[n]
+            G.node[n]['pos'] = (b.real, b.imag)
+            G.node[n]['b']   = b
 
-        #
-        # Compute paths and path indicators
-        #
-        paths = {}
-        paths = paths.fromkeys(H.edges())    
-        for i,j in paths.keys():
+        # add path indicators: the connectivity of the cirlces
+        for i,j in G.edges():
             d = np.real(disc_pts[j] - disc_pts[i])
             if   d >= radius:  ind = (1,-1)
             elif d <= -radius: ind = (-1,1)
             else:              ind = (-1,-1)
-            paths[(i,j)] = ind
+            G[i][j]['ind'] = ind
+        
+        return H
 
-        return H, disc_pts, paths, starting_vertex
-
-
-    def plot_initial_monodromy(self, kappa=1/2.9):
+    def _compute_monodromy_graph_vertex_types(G):
         """
-        Returns a plot of the initial monodromies of the discriminant points of an
+        Comptues the vertex types as defined in [FKS]:
+        
+        * 'node' -- a parent vertex of the graph with multiple children
+        * 'v-point' -- a vertex of special type described in Section 3 of [FKS]
+        * 'endpoint' -- a childless vertex
+
+        Note: the "children" of a vertex are neighbors that are further away 
+        from the starting vertex.
+        """
+        pass
+
+    def _compute_monodromy_graph_interpolating_functions():
+        """
+        Computes the parameterizations of the lines and circles of the
+        monodromy graph. Lines are stored under G.edge[i][j]['func']
+        and circles are stored under G.node[i]['func'].
+        """
+        pass
+
+    def show_paths(self):
+        """
+        Retudrns a plot of the initial monodromies of the discriminant points of an
         algebraic curve. Used for debugging purposes.
-
-        INPUTS:
-
-        - ``f``: a plane algebraic curve
-
-        - ``x``: the independent variable
-
-        - ``y``: the dependent variable
-
-        - ``kappa``: (default ``1/2.9``) a "relaxation coefficient" for 
-        determining the radius of the circles in the monodromy paths. 
-        ``kappa = 1`` produces touching circles, which in most cases is 
-        not computationally efficient.
-
-        - ``verbose``: (default ``False``) Set to ``True`` for debugging
-        statements
-
-        OUTPUTS:
-
-        - plot: a plot of the initial monodromy.
-
-
-        EXAMPLES:
         """
-        # compute discriminant points and radii
-        disc_pts = self.discriminant_points()
-        disc_pts = np.array(disc_pts, dtype=np.complex)
-        radius   = self.monodromy_radius(kappa=kappa)
-        ht = max(np.append(np.real(disc_pts), np.imag(disc_pts))) + radius
-
-
         # compute paths and path indices
-        H, disc_pts, paths, starting_vertex = self.initial_monodromy(kappa)
-        n = len(disc_pts)
+        H, starting_vertex = self.initial_monodromy(kappa=kappa)
+        n = H.number_of_nodes()
+        disc_pts = [H.node[n]['b'] for n in range(n)]
+        radius   = self.monodromy_radius(kappa=kappa)
+        ht = np.max(np.append(np.real(disc_pts), np.imag(disc_pts))) + radius
 
         # construct figure and axes
         fig = plt.figure()
@@ -422,138 +425,25 @@ class Monodromy:
         plt.axes([-ht,ht,-ht,ht])
         plt.show()
 
-        return None
 
+    def construct_interpolating_points(self, kappa=1/2.9):
+       """
+       Store 
+       """
+       H, starting_vertex = self.initial_monodromy(kappa=kappa)
+       
 
-    def interpolate_line(self, start, end, Npts=16):
-        """
-        Returns a list of uniformly interpolated points between the complex numbers
-        ``start`` and ``end``. The end point is not included.
-
-        INPUTS:
-
-        - ``start``: the starting point
-
-        - ``end``: the ending point
-
-        - ``Npts``: (default ``32``) the number of interpolating points to 
-        compute.
-
-        OUTPUTS:
-
-        - list: a list of interpolating points from ``start`` to ``end`` of
-        length ``Npts``
-
-        """
-        step = 1.0/Npts
-        pts  = range(Npts)
-        for n in range(Npts):
-            t = n * step
-            pts[n] = (1-t)*start + t*end
-
-        return pts
-
-
-
-    def interpolate_circle(self, center, radius, start, orient, Npts=32):
-        """
-        Returns a list of uniformly interpolated points between the complex numbers
-        ``start`` and ``end``. The end point is not included.
-
-        INPUTS:
-
-        - ``start``: the starting point
-
-        - ``end``: the ending point
-
-        - ``Npts``: (default ``32``) the number of interpolating points to 
-        compute.
-
-        OUTPUTS:
-
-        - list: a list of interpolating points from ``start`` to ``end`` of
-        length ``Npts``
-
-        """
-        if orient not in [-1,1]:
-            raise ValueError("Semicircle orientation must be -1 or 1.")
-
-        step = np.pi/(Npts)
-        pts  = range(Npts)
-        for n in range(Npts):
-            theta  = orient * n * step + start
-            pts[n] = np.complex(radius*exp(1.0j*theta) + center)
-
-        return pts
-
-
-
-
-    def interpolate_path(self, b, Npts=32):
-        H, disc_pts, paths, starting_vertex = self.inital_monodromy()
-        verts = H.shortest_path(starting_vertex,b)
-        path  = [(verts[i],verts[i+1]) for i in range(len(verts)-1)]
-        path_points = []
-
-        if path == []:
-            start = pi
-            pts = circ_interp(disc_pts[starting_vertex], radius, start=start, orient=1, Npts=Npts)
-            path_points.extend(pts)
-            pts = circ_interp(disc_pts[starting_vertex], radius, start=start+pi, orient=1, Npts=Npts)
-            path_points.extend(pts)
-        else:    
-            # 1)
-            side = -1    # use to keep track of where we were when computing where to go on the circles
-            for seg in path:
-                # obtain the path segment information
-                ind = paths[seg]          # which sides of the circle to travel to / from
-
-                # if the circle side we're starting on is not equal to the
-                # path index, create a circle (i.e. switch sides
-                if side != ind[0]:
-                    if side == -1: start = pi
-                    else:          start = 0
-                    pts = circ_interp(disc_pts[seg[0]], radius, start=start, orient=1, Npts=Npts)
-                    path_points.extend(pts)
-
-                # construct line:
-                pts = line_interp(disc_pts[seg[0]] + radius*ind[0], disc_pts[seg[1]] + radius*ind[1], Npts=Npts)
-                path_points.extend(pts)
-
-                # update which side of the circle we're on
-                side = ind[1]
-
-            return_path = [z for z in path_points]
-            return_path.reverse()
-
-
-            # 2) Encircle the target branch point
-            if ind[1] ==  1: start = 0
-            else:            start = pi
-            pts = circ_interp(disc_pts[seg[1]], radius, start=start, orient=1, Npts=Npts)
-            path_points.extend(pts)
-            pts = circ_interp(disc_pts[seg[1]], radius, start=start+pi, orient=1, Npts=Npts)
-            path_points.extend(pts)
-
-            # add the last point on the circle
-            path_points.append(disc_pts[seg[1]] + ind[1]*radius)
-
-
-            # 3) Add the return path and the base point
-            path_points.extend(return_path)
-            path_points.append(disc_pts[starting_vertex]-radius)
-
-        return path_points
-
+       
 
 
         
 
 
 if __name__=='__main__':
-    from sympy.abc import x,y
+   from sympy.abc import x,y
 
-    f = y**3 - 2*x**3*y - x**9
+   f = y**3 - 2*x**3*y - x**9
+   M = Monodromy(f,x,y)
 
 
 """
