da5179b1cf5109438bc38f2f2a38a4bea64c4c8d
Chris Swierczewski <cswiercz@gmail.com>
2012-10-05 15:27:01 -0700

    Integral basis work. Resolves Issue #10.
    Abelfunctions can now compute integral bases of algebraic
    function fields. The functionality is accessed via the
    'integralbasis' module. The primary function is
    'integral_basis'.
    
    An entry in the test suite still needs to be written.
    Additionally, the ability to compute Puiseux series up to
    a degree bound, as opposed to specifying a number of terms,
    will improve performance as well.

--- abelfunctions/integralbasis.py
+++ abelfunctions/integralbasis.py
@@ -1,14 +1,33 @@
+"""
+Integral Bases
+==============
+
+A module for computing integral bases of algebraic functions fields of
+the form C[x,y] / ( f(x,y) ). The algorithm is based off of the paper
+"An Algorithm for Computing an Integral Basis in an Algebraic Function
+Field" by Mark van Hoeij.
+
+Authors
+-------
+
+- Chris Swierczewski (Initial Version, October 2012)
+
+References
+----------
+
+[vH] Mark van Hoeij. "An Algorithm for Computing an Integral Basis in
+an Algebraic Function Field". J. Symbolic Computation. (1994) 18,
+p. 353-363
+
+"""
 import sympy
-import pdb 
 
 from puiseux import puiseux
-from sympy.abc import z
-
 
 def valuation(p,x):
     """
-    Given a collection of puiseux series, return the valuations. That is, the 
-    exponents of the leading order term.
+    Given a collection of Puiseux series, return the valuations. That
+    is, the exponents of the leading order term.
     """
     return p.expand(mult=True,force=True).leadterm(x)[1]
 
@@ -17,6 +36,8 @@ def Int(i,p):
     The function .. math:
 
         Int_i = \sum_{k \neq i} v(p_i - p_k)
+
+    for computing the Puiseux series bounds.
     """
     n   = len(p)
     pi  = p[i]
@@ -29,9 +50,10 @@ def Int(i,p):
 
 def compute_expansion_bounds(p,x):
     """
-    Computes the expansion bounds `N_1,\ldots,N_n` such that for all 
-    polynomials `G \in L[x,y]` the truncation `r_i` of the Puiseux series
-    `p_i` satisfying `v(r_i - p_i) > N_i` satisfies the relation .. math:
+    Computes the expansion bounds `N_1,\ldots,N_n` such that for all
+    polynomials `G \in L[x,y]` the truncation `r_i` of the Puiseux
+    series `p_i` satisfying `v(r_i - p_i) > N_i` satisfies the
+    relation ..math:
 
         \forall M \in \mathbb{N} \forall i v(G(r_i)) > M
 
@@ -39,19 +61,8 @@ def compute_expansion_bounds(p,x):
 
         \forall M \in \mathbb{N} \forall i v(G(r_i)) > M.
 
-    That is, the truncations `r_i` are sufficiently long so that polynomial
-    evaluation of `r_i` and `p_i` has the same valuation.
-
-    INPUT:
-
-        - ``v``: list of the valu
-
-        - ``x``: the independent variable in each of the `p`'s
-
-    OUTPUT:
-    
-        - ``(list)``: a list `[N_1,\ldots,N_n]` such that each `N_i` satisfies
-          the condition above.
+    That is, the truncations `r_i` are sufficiently long so that
+    polynomial evaluation of `r_i` and `p_i` has the same valuation.
     """
     n = len(p)
     N = []
@@ -66,9 +77,11 @@ def compute_expansion_bounds(p,x):
 
 def compute_series_truncations(f,x,y,a):
     """
-    Computes the Puiseux series expansions at the `x`-point `x=a` with the 
-    necessary number of terms in order to compute the integral basis of the 
-    algebraic functions field corresponding to `f`.
+    Computes the Puiseux series expansions at the `x`-point `x=a` with
+    the necessary number of terms in order to compute the integral
+    basis of the algebraic functions field corresponding to `f`. The
+    Puiseux series is returned in parametric form for computational
+    efficiency. (Sympy doesn't do as well with fractional exponents.)
     """
     # compute the first terms of the Puiseux series expansions
     p = puiseux(f,x,y,a,1,parametric=False)
@@ -81,22 +94,23 @@ def compute_series_truncations(f,x,y,a):
     # [[[XXX]]] this needs to be drastically improved, probably by adding the
     # option to compute Puiseux series up to a certain degree bound
     # instead of by number of terms.
-    z = sympy.Symbol('z')
-    r = puiseux(f,x,y,a,Nmax,parametric=False)
+    r = puiseux(f,x,y,a,Nmax,parametric=True)
+    T = iter(r[0][0].free_symbols).next()      # retrieving set elts is hard
     n = len(r)
-    for i in xrange(n):
-        ri = r[i]
-        ri = ri.subs(x,z+a)
-        ri = ri.expand(mul=True,force=True).series(z) + sympy.O(z**N[i])
-        r[i] = ri.removeO().subs(z,x-a)
 
-    return r
+    for i in xrange(n):
+        ri_X, ri_Y = r[i]
+        ramification_index = sympy.degree(ri_X, T)
+        ri_Y = ri_Y + sympy.O( T**(N[i]*ramification_index) )
+        r[i] = (ri_X, ri_Y.removeO())
+
+    return T, list(set(r))
 
 
 def integral_basis(f,x,y):
     """
-    Compute the integral basis of the polynomial `f` at the point `x=a`.
-
+    Compute the integral basis {b1, ..., bg} of the algebraic function
+    field C[x,y] / (f).
     """
     # If the curve is not monic then map y |-> y/lc(x) where lc(x)
     # is the leading coefficient of f
@@ -115,14 +129,14 @@ def integral_basis(f,x,y):
     factors = sympy.factor_list(res)[1]
     df = [k for k,deg in factors if (deg > 1) and (sympy.LC(k) == 1)]
 
-    # Here, r_{k,i} = r[k][i], the ith Puiseux series for the kth
-    # factor dividing the above resultant
+    # r[k][i] is the ith Puiseux series (in parametric form) for the
+    # kth factor dividing the above resultant
     alpha = []
     r = []
     for l in range(len(df)):
         k = df[l]
-        alphak = sympy.roots(k).keys()[0]  # pick a root of k
-        rk = compute_series_truncations(f,x,y,alphak)
+        alphak = sympy.roots(k).keys()[0]         # pick a root of k
+        T, rk = compute_series_truncations(f,x,y,alphak)
 
         alpha.append(alphak)
         r.append(rk)
@@ -130,7 +144,7 @@ def integral_basis(f,x,y):
     # Main Loop
     b = [1]
     for d in range(1,n):
-        # intiial guess for b_d
+        # intiial guess for b_d. Uses the trick of 
         bd = y*b[-1]
         a = sympy.symbols('a:%d'%d)
         for l in range(len(df)):
@@ -143,20 +157,29 @@ def integral_basis(f,x,y):
             found_something = True
             while found_something:
                 A = (sum(ak*bk for ak,bk in zip(a,b)) + bd) / (x - alphak)
-                #A = A.subs(x,z+alphak)
                 # construct system of equations consisting of the coefficients
                 # of negative powers of (x-alphak) in the substitutions
                 # A(r_{k,1}),...,A(r_{k,n})
                 equations = []
-                for rkl in rk:
-                    # [[XXX]] THE FOLLOWING TERM LOOP IS REALY SLOW
-                    lser = A.subs(y,rkl).expand(mult=True,force=True).lseries(x,alphak)
-                    for term in lser:
-                        coeff,deg = term.as_coeff_exponent(x-alphak)
-                        if deg < 0:
-                            equations.append(coeff)
-                        else:
-                            break
+
+                for rk_X, rk_Y in rk:
+                    # solve for T in terms of x to obtain the
+                    # coefficients for use in constucting the a_i
+                    # equations below
+                    #          alpha + mu T**q = x
+                    sols = sympy.solve((x-alphak)-rk_X,T)
+                    consts = map(lambda s: s.as_coeff_exponent(x-alphak)[0],
+                                 sols)
+                    
+                    # compute the series in T up to constant order (we
+                    # only need the coefficients of the T terms with
+                    # negative exponent
+                    ser = A.subs([(x,rk_X),(y,rk_Y)]).expand() + sympy.O(1)
+                    ser = ser.removeO()
+                        
+                    # use the constants computed above to generate the
+                    # equations that need to be solved.
+                    equations.extend([ser.subs(T,c) for c in consts])
 
                 # solve the equations for a0,...,a_{d-1}
                 sols = sympy.solve(equations,a)
@@ -169,10 +192,14 @@ def integral_basis(f,x,y):
                     else:
                         bdm1 = sum( sols[a[i]]*bk for i,bk in zip(range(d),b) )
                         bd = (bdm1 + bd) / k
+                        
+        # bd found. Append to list of basis elements
+        b.append( bd )
 
-        # after traversing V, append the resulting bd to the list of bs
-        # b.append( sy.simplify(bd*lc) )
-        b.append( sympy.simplify(bd.subs(y,y*lc)) )
+    # finally, convert back to singularized curve if necessary
+    for i in xrange(1,len(b)):
+        b[i] = b[i].subs(y,y*lc)
+
     return b
 
 
@@ -181,39 +208,22 @@ def integral_basis(f,x,y):
 if __name__=="__main__":
     from sympy.abc import x,y,T
 
-    f1 = (x**2 - x + 1)*y**2 - 2*x**2*y + x**4                   # yes *
+    f1 = (x**2 - x + 1)*y**2 - 2*x**2*y + x**4                   # yes
     f2 = -x**7 + 2*x**3*y + y**3                                 # yes
     f3 = (y**2-x**2)*(x-1)*(2*x-3) - 4*(x**2+y**2-2*x)**2        # yes
     f4 = y**2 + x**3 - x**2                                      # yes
-    f5 = (x**2 + y**2)**3 + 3*x**2*y - y**3                      # no (oerr)
-    f6 = y**4 - y**2*x + x**2                                    # no (wrong)
-    f7 = y**3 - (x**3 + y)**2 + 1
+    f5 = (x**2 + y**2)**3 + 3*x**2*y - y**3                      # yes
+    f6 = y**4 - y**2*x + x**2                                    # yes (long run time)
+    f7 = y**3 - (x**3 + y)**2 + 1                                # yes
 
-    f8 = (x**6)*y**3 + 2*x**3*y - 1                              # no (err)
+    f8 = (x**6)*y**3 + 2*x**3*y - 1                              # yes
     f9 = 2*x**7*y + 2*x**7 + y**3 + 3*y**2 + 3*y                 # yes
-    f10= (x**3)*y**4 + 4*x**2*y**2 + 2*x**3*y - 1                # no (wrong)
+    f10= (x**3)*y**4 + 4*x**2*y**2 + 2*x**3*y - 1                # yes
 
-    f = f10
+    f = f1
         
     print "Plane curve..."
     sympy.pprint(f)
-
-    print "\nComputing singular points..."
-    p = sympy.Poly(f,[x,y])
-    n = p.degree(y)
-    res = sympy.resultant(p,p.diff(y),y)
-    factors = sympy.factor_list(res)[1]
-    df = [k for k,deg in factors if (deg > 1) and (sympy.LC(k) == 1)]
-    print "\t resultant =", sympy.factor(res)
-    print "\t        df =", df
-    print "\t (monic irred. polynomials k s.t. k^2 | res)"
-
-    print "\nTruncated puiseux series expansions"
-    alpha = [0]
-    for alphak in alpha:
-        p = compute_series_truncations(f,x,y,alphak)
-        for rk in p:
-            sympy.pprint(rk)
 
     print "\nComputing integral basis..."
     b = integral_basis(f,x,y)
