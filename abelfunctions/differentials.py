4adfb62c89190289134f326a8cba16874b9b63bc
Chris Swierczewski <cswiercz@gmail.com>
2013-05-03 09:50:57 -0700

    Checkopint differentials.

--- abelfunctions/differentials.py
+++ abelfunctions/differentials.py
@@ -13,103 +13,35 @@ import sympy
 import sympy.mpmath as mpmath
 
 from abelfunctions.integralbasis import integral_basis
-from abelfunctions.singularities import singularities, _transform
+from abelfunctions.singularities import singularities, _transform, genus
 from abelfunctions.utilities import cached_function
 
 import pdb
 
-def _eliminate_higher_orders(h,x,y,deg,expr):
+def mnuk_conditions(f,x,y,b,P):
     """
-    Given a rational function `h = h(x,y) \in C(x)[y]`, replace all powers
-    of `y**deg` with `expr`.
-    """    
-    m = sympy.degree(h,y)
-    h = h.expand()
-    hred = sympy.S(0)
+    Determine the Mnuk conditions on the coefficients, c, of the general
+    adjoint polynomial P at the point x=0.
 
-    for term in h.as_ordered_terms():
-        term_deg = sympy.degree(term,y)
-        term = term.replace(sympy.Pow, 
-                            lambda arg, pow: arg**(pow-deg)*expr
-                            if arg == y and pow >= deg 
-                            else arg**pow)
-        hred += term
-
-    return hred.expand()
-
-
-
-def _solve_for_leading_y(f,x,y):
+    Note: it is assume t
     """
-    Given `f(x,y) = a_n(x) * y**n + ... + a_0(x)`, return
-    
-        (-a_{n-1}(x) - ... - a_o(x)) / a_n(x)
+    if isinstance(b,int): b = sympy.S(b)
+    numer, denom = b.ratsimp().as_numer_denom()
 
-    """
-    d = max(map(sum,f.as_poly(x,y).monoms()))
-    n = sympy.degree(f,y)
+    # reduce b*P modulo f
+    expr = numer.as_poly(x)*P.as_poly(x)
+    Q,R = sympy.polytools.reduced(expr.as_poly(y), [sympy.poly(f,y)])
 
-    sol = sympy.S(0)
-    a_n = sympy.S(1)
+    # divide by the largest power of x appearing in the denominator.
+    # this is sufficient since we've shifted the curve and its
+    # singularity to appear at 
+    denom = sympy.poly(denom, x)
+    try:    mult = sympy.roots(denom)[0]
+    except: mult = 0
+    rem = expr.as_poly(x).rem(sympy.poly(x**mult,x))
+    coeffs = rem.as_poly(x,y).coeffs()
 
-    for term in f.expand().as_ordered_terms():
-        if sympy.degree(term,y) == n:
-            a_n = term.coeff(y**n)
-        else:
-            sol -= term
-
-    return sol/a_n
-            
-
-def _adjoint_monomials(rhs,d,n,bi,P,c):
-    """
-    Obtain the adjoint monomials corresponding to the integral basis
-    element bi. This is done by demanding that
-
-        bi * P
-
-    be a polynomial where
-
-        P = sum( c_{ij} x**i y**j )
-
-    Input:
-    
-    - `rhs`: if ``f(x,y) = a_n(x)y^n + ... + a_1(x)y + a_0(x)`` then rhs
-    should be equal to ``(a_{n-1}(x)y^{n-1} + ... + a_0(x)) / a_n(x)``
-
-    - `d`: the total degree of ``f``
-    
-    - `n`: the degree of ``f`` in ``y``
-
-    - `bi`: an integral basis element
-
-    - `P`: the genreal adjoint polynomial
-
-    - `c`: the coefficient dictionary
-    """
-    pdb.set_trace()
-
-    # Compute b_i(x,y) * P(x,y), the product of the ith integral basis
-    # element with the general form of the adjoint
-    # polynomial. Substitute appropriately for y**n (n = deg_y(f)) as
-    # is occurs.
-    Pi = sympy.expand(bi * P)
-    Pi = _eliminate_higher_orders(Pi,x,y,n,rhs)
-
-    # Find all coefficients of terms with negative x-powers. Add the
-    # corresponding monomial to the monomials list.
-    p = sympy.Wild('p')
-    q = sympy.Wild('q')
-    coeff = sympy.Wild('coeff')
-    monoms = set([])
-    for term in Pi.as_ordered_terms(): 
-        if sympy.degree(x) < 0:
-            # strips out the cij from the monomial
-            cij = term.as_independent(x,y)[0].free_symbols.pop()
-            i,j = c[cij]
-            monoms.add(x**i*y**j)
-
-    return monoms
+    return coeffs
 
 
 def differentials(f,x,y):
@@ -128,29 +60,37 @@ def differentials(f,x,y):
     # of the degrees in both x and y.
     d = max(map(sum,f.as_poly(x,y).monoms()))
     n = sympy.degree(f,y)
-    rhs = _solve_for_leading_y(f,x,y)
 
-    # coeffiecients and general adjoint polynomial
-    c_arr = sympy.symarray('c',(d-2,d-2)).tolist()
-    c = dict( (cij,(c_arr.index(ci),ci.index(cij))) 
-              for ci in c_arr for cij in ci )
-    P = sum( c_arr[i][j]*x**i*y**j 
-             for i in range(d-2) for j in range(d-2) if i+j <= d-3)
+    # define the "generalized" adjoint polynomial.
+    c = sympy.symarray('c',(d-2,d-2)).tolist()
+    P = sum( c[i][j] * x**i * y**j 
+             for i in range(d-2) for j in range(d-2)
+             if i+j <= d-3)
+
+    # for each singular point [x:y:z] = [alpha:beta:gamma], map f onto
+    # the "most convenient and appropriate" affine subspace, (u,v),
+    # and center at u=0. determine the conditions on P
     S = singularities(f,x,y)
-
-    differentials = set([])
-    for (alpha, beta, gamma), (m,delta,r) in S:
+    conditions = []
+    for (alpha, beta, gamma),(m,delta,r) in S:
         g,u,v,u0,v0 = _transform(f,x,y,(alpha,beta,gamma))
-#        if delta > m*(m-1)/2:
-        if True:
-            # Use integral basis method.
-            b = integral_basis(g,u,v)
-            for bi in b:
-                monoms = _adjoint_monomials(f,x,y,bi,P,c)
-                differentials.union(monoms) 
-        else:
-            # Use Puiseux series method
-            P = puiseux(g,u,v,u0,nterms=0)
+        b = integral_basis(g,u,v)
+        g = g.subs(u,u+u0)
+        for bi in b:
+            conditions_bi = mnuk_conditions(g,u,v,bi.subs(u,u+u0),P)
+            conditions.extend(conditions_bi)
+
+    # solve the system of equations and retreive monomials contained
+    # in the general solution
+    c = [item for sublist in c for item in sublist]
+    sols = sympy.solve(conditions, c)
+    P = P.subs(sols).as_poly(x,y)
+    differentials = [x**i * y**j for (i,j) in P.monoms()]
+
+    # sanity check: the number of differentials matches the genus
+    g = genus(f,x,y)
+    if g != len(differentials):
+        raise AssertionError("Number of differentials does not match genus.")
 
 
     return [differential/sympy.diff(f,y) for differential in differentials]
@@ -172,9 +112,10 @@ if __name__=='__main__':
     f8 = x**6*y**3 + 2*x**3*y - 1
     f9 = 2*x**7*y + 2*x**7 + y**3 + 3*y**2 + 3*y
     f10= (x**3)*y**4 + 4*x**2*y**2 + 2*x**3*y - 1
+    f11= y**3 - x**3*y + 2*x**7
 
     fs = [f1,f2,f3,f4,f5,f6,f9,f10]
-    fs = [f2]
+    fs = [f11]
     
     for f in fs:
         print "Curve: f(x,y) = %s"%(f)
