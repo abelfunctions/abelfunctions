747f306e468f72b7a59797d3e421c6e84d50690b
Chris Swierczewski <cswiercz@gmail.com>
2016-02-29 11:18:46 -0800

    [#113] Define M_PI when not already defined
    In certain environments, the M_PI constant is not defined in the
    math.h header of the C standard library. From what I read it looks
    like M_PI was omitted past the C99 standard. In any case, a fix
    is introduced where the constant is explicitly defined.
    
    Fixes #113.

--- abelfunctions/riemann_theta/finite_sum.c
+++ abelfunctions/riemann_theta/finite_sum.c
@@ -23,6 +23,10 @@ extern "C" {
 #include <stdio.h>
 #include <math.h>
 
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+
 /******************************************************************************
   exppart
   -------
