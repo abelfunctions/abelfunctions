7bdaadfc8fe28ec02e4e2707808120fb100b5904
Robert Parini <robert.parini@gmail.com>
2018-01-05 16:41:02 +0000

    Remove build_ext
    Cythonize replaces Cython's custom build_ext

--- setup.py
+++ setup.py
@@ -24,7 +24,6 @@ import shutil
 import unittest
 from distutils.core import setup, Command
 from distutils.extension import Extension
-from Cython.Distutils import build_ext
 from Cython.Build import cythonize
 from numpy.distutils.misc_util import get_numpy_include_dirs
 
@@ -180,5 +179,5 @@ setup(
     packages = packages,
     ext_modules = cythonize(ext_modules),
     platforms = ['all'],
-    cmdclass = {'clean':clean, 'build_ext':build_ext}
+    cmdclass = {'clean':clean}
 )
