128560617e7920f9e5cd0b4e85dfa585751506f2
Frédéric Chapoton <fchapoton2@gmail.com>
2019-09-30 11:49:13 +0200

    python3 print

--- examples/riemannsurface_demo.py
+++ examples/riemannsurface_demo.py
@@ -1,35 +1,36 @@
-print "===== abelfunctions Demo Script ====="
+from __future__ import print_function
+print("===== abelfunctions Demo Script =====")
 from abelfunctions import *
 from sympy.abc import x,y
 
 #f = y**3 + 2*x**3*y - x**7
 f = y**2 - (x-1)*(x+1)*(x-2)*(x+2)
-X = RiemannSurface(f,x,y)
+X = RiemannSurface(f, x, y)
 
-print "\n\tRS"
-print X
+print("\n\tRS")
+print(X)
 
-print "\n\tRS: monodromy"
+print("\n\tRS: monodromy")
 base_point, base_sheets, branch_points, mon, G = X.monodromy()
-print "\nbase_point:"
-print base_point
-print "\nbase_sheets:"
-for s in base_sheets: print s
-print "\nbranch points:"
-for b in branch_points: print b
-print "\nmonodromy group:"
-for m in mon: print m
+print("\nbase_point:")
+print(base_point)
+print("\nbase_sheets:")
+for s in base_sheets: print(s)
+print("\nbranch points:")
+for b in branch_points: print(b)
+print("\nmonodromy group:")
+for m in mon: print(m)
 
-print "\n\tRS: homology"
+print("\n\tRS: homology")
 hom = X.homology()
-print "genus:"
-print hom['genus']
-print "cycles:"
-for c in hom['cycles']: print c
-print "lincomb:"
-print hom['linearcombination']
+print("genus:")
+print(hom['genus'])
+print("cycles:")
+for c in hom['cycles']: print(c)
+print("lincomb:")
+print(hom['linearcombination'])
 
-print "\n\tRS: computing cycles"
+print("\n\tRS: computing cycles")
 gamma = [X.c_cycle(i) for i in xrange(len(hom['cycles']))]
 
 # print "\n\tRS: period matrix"
@@ -39,5 +40,3 @@ gamma = [X.c_cycle(i) for i in xrange(len(hom['cycles']))]
 # print
 # print "abelfunctions: tau =", B[0][0]/A[0][0]
 # print "maple:         tau = 0.999999 + 1.563401 I"
-
-
