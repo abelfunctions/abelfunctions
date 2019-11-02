8dcd58e1b030464e34d2a6cd104d86d3760cfec2
Robert Parini <robert.parini@gmail.com>
2019-11-02 20:02:49 +0000

    Update CI tests (#183)
    Update CI tests

--- runtests.py
+++ runtests.py
@@ -49,58 +49,30 @@ def runtests(argv):
         if opt == '-p':
             processes = int(arg)
 
+    try:
+        import pytest
+    except ImportError as e:
+        raise ImportError(str(e)+"\n\n"
+            "Note: Running tests in parallel requires pytest-xdist.  Install with:\n\n"
+            "\t$ sage -pip install pytest-xdist\n\n"
+            "Note: If pip does not work because 'the ssl module in Python is not available' "
+            "then you may need to install openssl into sage and rebuild python using\n\n"
+            "\t$ sage -i openssl\n"
+            "\t$ sage -f python2"
+        )
+
+    # determine list of search patterns for tests
+    patterns = ' '.join(args)
+    pytest_args = ['-k', patterns, '--ignore=examples']
     if processes > 1:
-        try:
-            import pytest
-        except ImportError as e:
-            raise ImportError(str(e)+"\n\n"
-                "Note: Running tests in parallel requires pytest-xdist.  Install with:\n\n"
-                "\t$ sage -pip install pytest-xdist\n\n"
-                "Note: If pip does not work because 'the ssl module in Python is not available' "
-                "then you may need to install openssl into sage and rebuild python using\n\n"
-                "\t$ sage -i openssl\n"
-                "\t$ sage -f python2"
-            )
+        pytest_args += ['-n', str(processes)]
 
-        # determine list of search patterns for tests
-        patterns = ''
-        for arg in args:
-            patterns += str(arg) + ' '
-        patterns = patterns[:-1]
+    # highlight the runtimes for the 5 slowest tests
+    pytest_args.append('--durations=5')
 
-        pytest_args = ['-k', patterns, '--ignore=examples', '-n', str(processes)]
-
-        # highlight the runtimes for the 5 slowest tests
-        pytest_args.append('--durations=5')
-
-        if verbosity == 2:
-            pytest_args.append('-v')
-        errno = pytest.main(pytest_args)
-
-    else:
-        # determine list of search patterns for tests
-        patterns = []
-        for arg in args:
-            # if the string 'test' doesn't appear in the pattern argument then
-            # prepend to string
-            pattern = arg
-            if not 'test' in pattern:
-                pattern = 'test*' + pattern + '*'
-            if not '.py' in pattern:
-                pattern = pattern + '.py'
-            patterns.append(pattern)
-        if not patterns:
-            patterns = ['test*.py']
-
-        # run tests for each requested pattern
-        start_dir = 'abelfunctions'
-        for pattern in patterns:
-            loader = unittest.TestLoader()
-            suite = loader.discover(start_dir, pattern=pattern)
-            runner = unittest.TextTestRunner(verbosity=verbosity)
-            result = runner.run(suite)
-            errno = not result.wasSuccessful()
-
+    if verbosity == 2:
+        pytest_args.append('-v')
+    errno = pytest.main(pytest_args)
     sys.exit(errno)
 
 if __name__ == '__main__':    
