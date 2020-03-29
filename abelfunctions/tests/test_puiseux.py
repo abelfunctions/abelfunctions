78e602904e4289788b0f4695e372a55827a3a880
Robert Parini <robert.parini@gmail.com>
2020-03-29 15:01:48 +0100

    Fix six.assertCountEqual call
    Needs the testing instance

--- abelfunctions/tests/test_puiseux.py
+++ abelfunctions/tests/test_puiseux.py
@@ -333,7 +333,7 @@ class TestPuiseux(AbelfunctionsTestCase):
         x,y = self.f1.parent().gens()
         x = QQbar['x,y'](x)
         y = QQbar['x,y'](y)
-        six.assertCountEqual(
+        six.assertCountEqual(self, 
             series,
             [(x**2, x**4*(x*(y + 1) + 1))])
 
@@ -342,7 +342,7 @@ class TestPuiseux(AbelfunctionsTestCase):
         x,y = self.f2.parent().gens()
         x = QQbar['x,y'](x)
         y = QQbar['x,y'](y)
-        six.assertCountEqual(
+        six.assertCountEqual(self, 
             series,
             [(x, x**2*y),
              (-x**2/2, -x**3*(y + 1)/2)])
@@ -352,7 +352,7 @@ class TestPuiseux(AbelfunctionsTestCase):
         x,y = self.f2.parent().gens()
         x = QQbar['x,y'](x)
         y = QQbar['x,y'](y)
-        six.assertCountEqual(
+        six.assertCountEqual(self, 
             series,
             [(1/x**3, (x**2*y + x**2)/x**9)])
 
@@ -365,7 +365,7 @@ class TestPuiseux(AbelfunctionsTestCase):
         x,y = self.f4.parent().gens()
         x = QQbar['x,y'](x)
         y = QQbar['x,y'](y)
-        six.assertCountEqual(
+        six.assertCountEqual(self, 
             series,
             [(x, x*(y + 1)),
              (x, x*(y - 1))])
@@ -375,7 +375,7 @@ class TestPuiseux(AbelfunctionsTestCase):
         x,y = self.f4.parent().gens()
         x = QQbar['x,y'](x)
         y = QQbar['x,y'](y)
-        six.assertCountEqual(
+        six.assertCountEqual(self, 
             series,
             [(1/(-x**2), (x*y + x)/x**4)])
 
@@ -388,7 +388,7 @@ class TestPuiseux(AbelfunctionsTestCase):
         x,y = self.f7.parent().gens()
         x = QQbar['x,y'](x)
         y = QQbar['x,y'](y)
-        six.assertCountEqual(
+        six.assertCountEqual(self, 
             series,
             [(x, y + r0),
              (x, y + r1),
@@ -399,7 +399,7 @@ class TestPuiseux(AbelfunctionsTestCase):
         x,y = self.f22.parent().gens()
         x = QQbar['x,y'](x)
         y = QQbar['x,y'](y)
-        six.assertCountEqual(
+        six.assertCountEqual(self, 
             series,
             [(x**3, x**5*(y + 1))])
 
@@ -408,7 +408,7 @@ class TestPuiseux(AbelfunctionsTestCase):
         x,y = self.f2.parent().gens()
         x = QQbar['x,y'](x)
         y = QQbar['x,y'](y)
-        six.assertCountEqual(
+        six.assertCountEqual(self, 
             series,
             [(1/x**3, (x*y + x)/x**6)])
 
@@ -417,7 +417,7 @@ class TestPuiseux(AbelfunctionsTestCase):
         x,y = self.f23.parent().gens()
         x = QQbar['x,y'](x)
         y = QQbar['x,y'](y)
-        six.assertCountEqual(
+        six.assertCountEqual(self, 
             series,
             [(x, x*(x*y + 2) + 1),
              (x, x*(x*(y + 1) + 2) + 1)])
@@ -427,7 +427,7 @@ class TestPuiseux(AbelfunctionsTestCase):
         x,y = self.f23.parent().gens()
         x = QQbar['x,y'](x)
         y = QQbar['x,y'](y)
-        six.assertCountEqual(
+        six.assertCountEqual(self, 
             series,
             [(1/x,y/x**7), (1/x,(1+y)/x**7)])
 
@@ -440,7 +440,7 @@ class TestPuiseux(AbelfunctionsTestCase):
         x,y = self.f27.parent().gens()
         x = QQbar['x,y'](x)
         y = QQbar['x,y'](y)
-        six.assertCountEqual(
+        six.assertCountEqual(self, 
             series,
             [(x, x*(y + sqrt2)),
              (x, x*(y - sqrt2)),
@@ -454,7 +454,7 @@ class TestPuiseux(AbelfunctionsTestCase):
         series = self.get_PQ(f,a=infinity)
         x = QQbar['x,y'](x)
         y = QQbar['x,y'](y)
-        six.assertCountEqual(
+        six.assertCountEqual(self, 
             series,
             [(1/x, (y+1)/x**3),
              (1/x, (y-1)/x**3)])
