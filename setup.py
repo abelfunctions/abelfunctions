e35344c8cf9d01de7b5a1434d21c976d8579d4c4
Robert Parini <robert.parini@gmail.com>
2022-09-11 01:18:48 +0100

    Lower python requirement to 3.6

--- setup.py
+++ setup.py
@@ -174,7 +174,7 @@ setup(
     packages = packages,
     install_requires=['six'],
     ext_modules = cythonize(ext_modules),
-    python_requires=">=3.8",
+    python_requires=">=3.6",
     platforms = ['all'],
     cmdclass = {'clean':clean}
 )
