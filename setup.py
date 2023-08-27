3116fb100bd2a730ec81f2e856c878f7e2c47ed3
Robert Parini <robert.parini@gmail.com>
2023-08-27 19:12:02 +0100

    Fix pip install on ubuntu apt sage (#219)
    * Remove SAGE_ROOT
    
    * Test against apt-install sage
    
    * test pip install
    
    * Update test.yml
    
    * Revert "Remove SAGE_ROOT"
    
    This reverts commit 70909c56e826564e5437b5a6e175a89a69fb4d69.
    
    * Update test.yml
    
    * Revert "Revert "Remove SAGE_ROOT""
    
    This reverts commit f7a25727191bea691d7f3a703114597b9270d05e.

--- setup.py
+++ setup.py
@@ -36,8 +36,7 @@ except ImportError:
 # raise error if the user is not using Sage to compile
 try:
     from sage.env import sage_include_directories
-    SAGE_ROOT = os.environ['SAGE_ROOT']
-except (ImportError, KeyError):
+except ImportError:
     raise EnvironmentError('abelfunctions must be built using Sage:\n\n'
                            '\t$ sage setup.py <args> <kwds>\n')
 
