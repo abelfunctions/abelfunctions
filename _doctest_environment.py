527f2aed52855ca526a4c7a04f5a002368e6ac55
Matthias Köppe <mkoeppe@math.ucdavis.edu>
2025-01-12 11:20:35 -0800

    _doctest_environment.py: Add # noqa
    Co-authored-by: Robert Parini <robert.parini@gmail.com>

--- _doctest_environment.py
+++ _doctest_environment.py
@@ -1,4 +1,4 @@
 # Toplevel for doctesting with passagemath
 
-from sage.all__sagemath_modules import *
-from sage.all__sagemath_symbolics import *
+from sage.all__sagemath_modules import *  # noqa: F403
+from sage.all__sagemath_symbolics import *  # noqa: F403
