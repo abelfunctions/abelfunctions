5ba76be454cd3932cd8386c5074c06b7da53b4d8
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2025-04-01 21:59:55 -0700

    abelfunctions/puiseux_series_ring_element.pyx: Use try..except for import for Sage 9.x

--- abelfunctions/puiseux_series_ring_element.pyx
+++ abelfunctions/puiseux_series_ring_element.pyx
@@ -105,7 +105,10 @@ from sage.ext.fast_callable import fast_callable
 from sage.rings.big_oh import O
 from sage.rings.integer_ring import Z as ZZ
 from sage.rings.rational_field import Q as QQ
-from sage.rings.cc import CC
+try:
+    from sage.rings.cc import CC
+except ImportError:
+    from sage.rings.all import CC
 from sage.rings.infinity import infinity
 from sage.rings.laurent_series_ring_element import is_LaurentSeries
 from sage.rings.laurent_series_ring_element cimport LaurentSeries
