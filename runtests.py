a5d0b9bc856f1b2de667cd1ed0a47e0b0c55c2cc
Robert Parini <robert.parini@gmail.com>
2025-08-30 22:01:39 +0100

    Get stdout

--- runtests.py
+++ runtests.py
@@ -64,7 +64,7 @@ def runtests(argv):
 
     # determine list of search patterns for tests
     patterns = " ".join(args)
-    pytest_args = ["-W error", "-k", patterns, "--ignore=examples", "-x"]
+    pytest_args = ["-W error", "-k", patterns, "--ignore=examples", "-x", "-s"]
     if processes > 1:
         pytest_args += ["-n", str(processes)]
 
