e75ad53c79701a3d2673395ea5f920c17be28cd4
Robert Parini <robert.parini@gmail.com>
2026-06-27 07:39:52 +0100

    Downgrade UserWarning to warning

--- runtests.py
+++ runtests.py
@@ -64,7 +64,13 @@ def runtests(argv):
 
     # determine list of search patterns for tests
     patterns = " ".join(args)
-    pytest_args = ["-W error", "-k", patterns, "--ignore=examples"]
+    pytest_args = [
+        "-W error",
+        "-W default::UserWarning",
+        "-k",
+        patterns,
+        "--ignore=examples",
+    ]
     if processes > 1:
         pytest_args += ["-n", str(processes)]
 
